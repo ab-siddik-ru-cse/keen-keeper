92038a97f0970c45d02887e297f81c1fea7fbed4
ab-siddik-ru-cse <mdabubokkor1024@gmail.com>
2026-04-13 23:14:17 +0600

    custom 404 page added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import { Home, Clock, BarChart2, Menu } from 'lucide-react';
+import Link from 'next/link';
 
 const NavBar = () => {
+    const [active, setActive] = useState(false);
+    const handleActive = () => {
+        setActive(true);
+    }
     const navLinks = [
-        { name: 'Home', icon: <Home size={18} />, href: '#', active: true },
-        { name: 'Timeline', icon: <Clock size={18} />, href: '#', active: false },
-        { name: 'Stats', icon: <BarChart2 size={18} />, href: '#', active: false },
+        { name: 'Home', icon: <Home size={18} />, href: '/'},
+        { name: 'Timeline', icon: <Clock size={18} />, href: '/timeline' },
+        { name: 'Stats', icon: <BarChart2 size={18} />, href: '/stats'},
     ];
 
     return (
@@ -23,38 +29,40 @@ const NavBar = () => {
                 {/* Desktop Menu */}
                 <div className="hidden lg:flex items-center gap-2">
                     {navLinks.map((link) => (
-                        <a
+                        <Link
+                            onClick={handleActive}
                             key={link.name}
                             href={link.href}
-                            className={`flex items-center gap-2 px-4 py-2 rounded-lg transition-all font-medium ${link.active
-                                    ? 'bg-emerald-900 text-white shadow-md' // Active State
-                                    : 'text-slate-500 hover:bg-gray-100'     // Inactive State
+                            className={`flex items-center gap-2 px-4 py-2 rounded-lg transition-all font-medium ${active
+                                ? 'bg-emerald-900 text-white shadow-md' // Active State
+                                : 'text-slate-500 hover:bg-gray-100'     // Inactive State
                                 }`}
                         >
                             {link.icon}
                             {link.name}
-                        </a>
+                        </Link>
                     ))}
                 </div>
 
                 {/* Mobile Dropdown (daisyUI) */}
                 <div className="dropdown dropdown-end lg:hidden">
-                    <label tabIndex={0} className="btn btn-ghost">
+                    <label tabIndex={0} className="btn btn-primary">
                         <Menu size={24} />
                     </label>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 border border-gray-100"
+                        className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52 border border-gray-100"
                     >
                         {navLinks.map((link) => (
                             <li key={link.name}>
-                                <a
+                                <Link
+                                    onClick={handleActive}
                                     href={link.href}
-                                    className={link.active ? 'bg-emerald-900 text-white' : ''}
+                                    className={link.active ? 'bg-emerald-900 text-white' : 'text-slate-500 hover:bg-gray-100'}
                                 >
                                     {link.icon}
                                     {link.name}
-                                </a>
+                                </Link>
                             </li>
                         ))}
                     </ul>
