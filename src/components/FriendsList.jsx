510eb74e40d194ac399c7ccfcaf0fb3c0e6b8225
ab-siddik-ru-cse <mdabubokkor1024@gmail.com>
2026-04-17 15:56:35 +0600

    some issues fix

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -8,8 +8,8 @@ const FriendsList = () => {
     const { friends, isLoading } = useContext(AppContext);
     return (
         <section className='bg-[#F8FAFC]'>
-            <div className="max-w-7xl mx-auto px-4 py-12">
-                <h2 className="text-2xl font-bold text-slate-800 mb-8 px-2">Your Friends</h2>
+            <div className="max-w-7xl mx-auto py-12">
+                <h2 className="text-2xl font-bold text-slate-800 mb-8">Your Friends</h2>
 
                 {/* Responsive tailwind css style */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
